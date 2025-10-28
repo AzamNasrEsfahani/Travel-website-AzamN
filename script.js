6dba7c38f83f5c27e0e2658e2c93e5636dd2c617
AzamNasrEsfahani <annima1361@gmail.com>
2025-10-28 16:48:14 -0400

    Add Leaflet html, js

--- script.js
+++ script.js
@@ -6,3 +6,17 @@ new Glide('.glide', {
   autoplay: 4000,
   perView: 1
 }).mount();
+// Leaflet map showing Iran 
+var iranBounds = [[25.0, 44.0], [40.0, 63.0]]; // SW lat-lon, NE lat-lon
+
+var map = L.map('mapid');
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; OpenStreetMap contributors'
+}).addTo(map);
+
+map.fitBounds(iranBounds);
+
+// Optional marker for Tehran
+L.marker([35.6892, 51.3890]).addTo(map)
+  .bindPopup('Tehran, Iran')
+  .openPopup();
